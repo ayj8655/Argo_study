bdaa470709c3c80b0d291de5a6d4cc0104592eee
ayj <ayj@DESKTOP-OPFRUL5>
2021-03-05 17:54:32 +0900

    210305

---
+++ Argo_study/src/baekjoon/gold/B_17135_캐슬디펜스.java
@@ -0,0 +1,16 @@
+package baekjoon.gold;
+
+import java.util.Scanner;
+
+public class B_17135_캐슬디펜스 {
+
+	static int N,M,D;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		D = sc.nextInt();
+	}
+
+}
