3d09e2e5bdeb70fdad94a0db63cb0af91c929ce2
ayj <ayj@DESKTOP-OPFRUL5>
2021-03-08 00:50:51 +0900

    210308

---
+++ Argo_study/src/baekjoon/gold/B_20056_마법사상어와파이어볼.java
@@ -0,0 +1,16 @@
+package baekjoon.gold;
+
+import java.util.Scanner;
+
+public class B_20056_마법사상어와파이어볼 {
+	static int N,M,K;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		K = sc.nextInt();
+		
+		
+	}
+
+}
