95513987b87669078cbdd0c4c6fb1d16142f3d2c
ayj8655 <ayj8655@naver.com>
2021-03-09 02:30:47 +0900

    210309

--- Argo_study/src/baekjoon/gold/B_17135_캐슬디펜스.java
+++ Argo_study/src/baekjoon/gold/B_17135_캐슬디펜스.java
@@ -1,17 +1,15 @@
 package baekjoon.gold;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class B_17135_캐슬디펜스 {
 
-	static int N,M,D;
+	static int N,M,D,Ans;
 	static int arr [][];		//게임판
 	static int copyarr[][];		//게임판원본
-	static boolean v[];			//죽일 타겟 위치
-	static int sel [];
-	static int com[];
+	static int sel [];	//조합저장용
+	static int com[];	//조합생성용
 	
 	
 	static class Point{
@@ -23,7 +21,7 @@ public class B_17135_캐슬디펜스 {
 		}
 	}
 	
-	static LinkedList<Point> enemy;
+	static LinkedList<Point> enemy;//적 위치 저장용
 	
 	
 	public static void main(String[] args) {
@@ -34,7 +32,6 @@ public class B_17135_캐슬디펜스 {
 		
 		arr = new int[N+1][M];
 		copyarr = new int[N+1][M];
-		v = new boolean[M];
 		sel = new int[3];
 		com = new int[M];
 		
@@ -53,14 +50,11 @@ public class B_17135_캐슬디펜스 {
 		//궁수 배치 조합으로
 		combi(0,0);
 		
+		System.out.println(Ans);
 		//print();
 		
 	}
-	
-	private static int dis(Point a, Point e) {
-		return Math.abs(a.y-e.y) + Math.abs(a.x-e.x);
-	}
-	
+
 	private static void print() {
 		for (int i = 0; i <= N; i++) {
 			for (int j = 0; j < M; j++) {
@@ -82,7 +76,6 @@ public class B_17135_캐슬디펜스 {
 			
 			//뽑힌 조합에서 최대 수 나오게 계산하면 끝
 			init();	// 매번 계산할 때 마다 배열 초기화
-			enemy = new LinkedList<>();	//적의 위치 저장용 리스트
 			
 			fight();
 			
@@ -98,14 +91,66 @@ public class B_17135_캐슬디펜스 {
 	}
 
 	private static void fight() {
-		int cnt=0;
 		
-		while(true) {
-			for (int i = 0; i < arr.length; i++) {
+		int cnt=0;
+		int round=0;
+		enemy = new LinkedList<>();	//적의 위치 저장용 리스트
+		
+		while(round<N) {
+			for (int i = 0; i < 3; i++) {	//각 각의 아처들 한번씩 돌리기
+				int x = N-round;		//행
+				int y = sel[i];		//선택된 아처 위치
+				int min = Integer.MAX_VALUE;
+				int minx = -1;
+				int miny = -1;
+				boolean flag = false;	//적을 찾았는지 확인
 				
+				for (int j = N-1-round; j >=0; j--) {//맨 아래부터 탐색
+					
+					for (int k = 0; k < M; k++) {
+						
+						if(arr[j][k]==1) {		//만약 적이 있다면?
+							
+							int dis = Math.abs(j-x) + Math.abs(k-y);
+							
+							if(dis<=D) {		//적의 거리가 유효 사거리 안이라면?
+								
+								if(dis < min) {	//최소거리 갱신
+									flag =true;
+									min =dis;
+									minx =j;
+									miny =k;
+								}
+								else if(dis==min) {	//거리가 같다면 왼쪽으로
+									if(miny>k) {
+										minx=j;
+										miny=k;
+										
+									}
+								}
+							}
+						}
+					}
+				}
+
+				if(flag) enemy.add(new Point(minx,miny));	//상대가 있다면 추가
+
 			}
 			
+			while(!enemy.isEmpty()) {	//죽일 상대 하나씩 꺼내서 제거
+				Point p = enemy.poll();
+				if(arr[p.x][p.y] == 1) {
+					arr[p.x][p.y] = 0;
+					cnt++;
+				}
+			}
+			
+			round++;
+			
+			
 		}
+		
+		Ans = Math.max(cnt, Ans);
 		
 	}
 }
--- Argo_study/src/baekjoon/gold/B_17136_색종이붙이기.java
+++ Argo_study/src/baekjoon/gold/B_17136_색종이붙이기.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class B_17136_색종이붙이기 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	//내 생각 -> 그리디하게 5부터 가능한거 찾고 없으면 4 3 2 1 순으로 해보기
 		Scanner sc = new Scanner(System.in);
 		
 
