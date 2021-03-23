61ac5442fe6345f2cb673f19bdfa1b4c6c15a381
ayj8655 <ayj8655@naver.com>
2021-03-23 12:42:25 +0900

    210323

---
+++ Argo_study/src/baekjoon/bronze/B_2750_수정렬하기.java
@@ -0,0 +1,31 @@
+package baekjoon.bronze;
+
+import java.util.*;
+import java.io.*;
+
+public class B_2750_수정렬하기 {
+	
+	static int N;
+	static int arr[];
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_B_2750.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		N = Integer.parseInt(br.readLine());	
+		
+		arr = new int [N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());	
+		}
+		
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < N; i++) {
+			sb.append(arr[i]).append("\n");
+		}
+		
+		System.out.print(sb.toString());
+	}
+	
+
+}
