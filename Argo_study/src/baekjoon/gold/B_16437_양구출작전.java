2b06aafd9295b72bfe92dc3ab6f90e87b97691e2
ayj8655 <ayj8655@naver.com>
2021-04-14 01:24:56 +0900

    210414

---
+++ Argo_study/src/baekjoon/gold/B_16437_양구출작전.java
@@ -0,0 +1,12 @@
+package baekjoon.gold;
+
+public class B_16437_양구출작전 {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+
+	}
+
+}
--- Argo_study/src/baekjoon/gold/B_17951_흩날리는시험지속에서내평점이느껴진거야.java
+++ Argo_study/src/baekjoon/gold/B_17951_흩날리는시험지속에서내평점이느껴진거야.java
@@ -1,11 +1,62 @@
 package baekjoon.gold;
 
+import java.io.*;
+import java.util.*;
+
+
 public class B_17951_흩날리는시험지속에서내평점이느껴진거야 {
 
-	public static void main(String[] args) {
+	static int N,K,Ans;
+	static int arr[];
+	
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_B_17951.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());		
+		arr = new int [N];
+		st = new StringTokenizer(br.readLine()," ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 		
+		int left =0;
+		int right = N*20;
+		
+		while (left <= right) {
+			int mid = (left+right) / 2;
+			
+			if(isPossible(mid)) 
+				left = mid +1;
+			else 
+				right = mid-1;
+		}
+		
+		System.out.println(Ans);
+	}
 
+	private static boolean isPossible(int mid) {
+		
+		int score = 0;
+		int tem = Integer.MAX_VALUE;
+		int cnt = 1;
+		for (int i = 0; i < N; i++) {
+			if (score + arr[i] >= mid) {
+				score += arr[i];
+				tem = Math.min(tem, score);
+				score = 0;
+				cnt++;
+				if (cnt > K) {
+					Ans = Math.max(Ans, tem);
+					return true;
+				}
+			}
+			else 
+				score += arr[i];
+		}
+		return false;
 	}
 
 }
