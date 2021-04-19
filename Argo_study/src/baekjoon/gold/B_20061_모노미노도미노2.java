e8c41098b0e40d111be6dfc14771fa987ae910c8
ayj8655 <ayj8655@naver.com>
2021-04-20 00:21:31 +0900

    210420

---
+++ Argo_study/src/baekjoon/gold/B_20061_모노미노도미노2.java
@@ -0,0 +1,11 @@
+package baekjoon.gold;
+
+public class B_20061_모노미노도미노2 {
+
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- Argo_study/src/swea/d5/S_3238_이항계수구하기_pro.java
+++ Argo_study/src/swea/d5/S_3238_이항계수구하기_pro.java
@@ -6,56 +6,58 @@ import java.io.*;
 
 public class S_3238_이항계수구하기_pro {
 
-	public static int iT=0,N,R,P;
-	public static int Q=11;
+	static int P;
+	 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-	
-	static long n,r;
-	
-	public static void main(String[] args) throws Exception {
-		Scanner scann=new Scanner(System.in);
-		iT=scann.nextInt();
-		for (int T = 1; T <= iT; T++) {
+		int T = Integer.parseInt(br.readLine());
 
-			n=scann.nextLong();
-			r=scann.nextLong();
-			P=scann.nextInt();
-			
-			
-//			System.out.printf("#%d %d\n",T,nCr(n,r,P));
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer tokens = new StringTokenizer(br.readLine());
+			long N = Long.parseLong(tokens.nextToken());
+			long R = Long.parseLong(tokens.nextToken());
+			P = Integer.parseInt(tokens.nextToken());
+
+			long[] factorial = new long[P + 1];
+			factorial[0] = 1;
+			for (int i = 1; i <= P; i++) {
+				factorial[i] = (factorial[i - 1] * i) % P;
+			}
+
+			long answer = 1;
+
+			while (N > 0 && R > 0) {
+				long n = N % P;
+				long r = R % P;
+				if (n < r) {
+					answer = 0;
+					break;
+				}
+				answer *= factorial[(int) n];
+				answer *= pow((factorial[(int) (n - r)] * factorial[(int) r]) % P, P - 2) % P;
+				answer %= P;
+				N /= P;
+				R /= P;
+			}
+			sb.append("#").append(t).append(" ").append(answer).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static long pow(long a, long b) {
+		if (b == 0)
+			return 1;
+
+		long n = pow(a, b / 2);
+
+		if (b % 2 == 0) {
+			return (n * n) % P;
+
+		} else {
+			return ((n * n) % P * a) % P;
+
 		}
 	}
-	static long power(long x, long y, long p)  { 
-		long res = 1L; 
-        x = x % p; 
-        //=> 3^7 > 7 3 1   3^7 --> 3^1*3^2*3^4             
-        while (y > 0) {    
-            if (y % 2 == 1) 
-                res = (res * x) % p; 
-            y = y >> 1; // y = y/2 
-            x = (x * x) % p; 
-        }  
-        return res; 
-    } 
-	
-    static long modInverse(long n, long p) { 
-        return power(n, p-2, p); 
-    }
-    
-    
-    static long nCr(int n, int r, int p) { 
-        if (r == 0) 
-            return 1L; 
-
-        long[] fac = new long[n+1]; 
-        fac[0] = 1; 
-          
-        for (int i = 1 ;i <= n; i++) 
-            fac[i] = fac[i-1] * i % p; 
-      
-        return (fac[n]* power(fac[r], p-2,p ) 
-                % p * power(fac[n-r], p-2,p) 
-                                    % p) % p; 
-    } 
 }
-
