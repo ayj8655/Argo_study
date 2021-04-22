ea5adaf5df3a43e1eb1cbe0b9c133839d2a43d93
ayj8655 <ayj8655@naver.com>
2021-04-22 17:38:09 +0900

    210422

---
+++ Argo_study/src/baekjoon/gold/B_1756_피자굽기.java
@@ -0,0 +1,17 @@
+package baekjoon.gold;
+
+import java.util.*;
+
+import java.io.*;
+
+public class B_1756_피자굽기 {
+
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_B_1756.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+	}
+
+}
---
+++ Argo_study/src/baekjoon/gold/B_17825_주사위윷놀이.java
@@ -0,0 +1,17 @@
+package baekjoon.gold;
+
+import java.util.*;
+
+import java.io.*;
+
+public class B_17825_주사위윷놀이 {
+
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_B_17825.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+	}
+
+}
