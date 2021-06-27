0eee0760dc40e702c5239bfa9ef39466620df33f
ayj8655 <ayj8655@naver.com>
2021-06-27 18:10:02 +0900

    210627

---
+++ Argo_study/src/baekjoon/bronze/B_2741_N찍기.java
@@ -0,0 +1,29 @@
+package baekjoon.bronze;
+
+import java.io.*;
+import java.util.*;
+
+
+public class B_2741_N찍기 {
+
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		System.setIn(new FileInputStream("res/input_B_2741.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		
+		int temp = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= temp; i++) 
+			sb.append(i+"\n");
+		
+		
+		System.out.println(sb);
+		
+
+	}
+
+}
