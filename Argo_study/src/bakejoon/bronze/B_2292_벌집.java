5e87d0b69194330ec314abc9a880be3955b40e83
ayj8655 <ayj8655@naver.com>
2021-12-29 01:01:06 +0900

    211229

--- Argo_study/src/bakejoon/bronze/B_2292_벌집.java
+++ Argo_study/src/bakejoon/bronze/B_2292_벌집.java
@@ -4,36 +4,30 @@ import java.util.*;
 import java.io.*;
 
 public class B_2292_벌집 {
-	
-	
-	
-	public static void main(String[] args) throws Exception{
-		
+
+	// 6의 배수로 늘어난다.
+	public static void main(String[] args) throws Exception {
+
 		System.setIn(new FileInputStream("res/input_B_2292.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
+
 		int N = Integer.parseInt(br.readLine());
 
-		int num = 1;
-		int range = 2;
-		
-		if(N == 1) {
+		int num = 1; // 최소 루트
+		int range = 2; // 범위 (최소값기준)
+
+		if (N == 1) {
 			System.out.print("1");
 		}
-		
+
 		else {
-			while(range <= N ) {
-				range = range + (6*num);
+			while (range <= N) { // 범위가 N보다 커지기 전까지ㅁ
+				range += (6 * num); // 다음 범위의 최솟값으로 변경
 				num++;
 			}
+			System.out.print(num);
 		}
-		
-		System.out.print(num);
-		
-		
-		
-		
+
 	}
 
 }
